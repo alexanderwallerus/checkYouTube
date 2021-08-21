02ebc956ada9008d571a0b7e26dadf7834fd1a37
alexanderwallerus <alex.wallerus@gmail.com>
2021-08-21 23:34:02 +0200

    resilience against title changes

--- checkYouTube.py
+++ checkYouTube.py
@@ -27,10 +27,15 @@ def getVidFromChannel(channelUrl):
         #print('no cookie banner in the way')
         pass
     
-    xPath = '//div[@class="style-scope ytd-grid-video-renderer"]'
-    firstVidDiv = driver.find_element_by_xpath(xPath)
-    vidName = firstVidDiv.find_element_by_xpath('//a[@id="video-title"]')
-    return vidName.text
+    firstVidxPath = '//div[@class="style-scope ytd-grid-video-renderer"]'
+    firstVidDiv = driver.find_element_by_xpath(firstVidxPath)
+    vidTitle = firstVidDiv.find_element_by_xpath('.//a[@id="video-title"]')
+    
+    #get the video's unique hash from the end of its url
+    vidHash = vidTitle.get_attribute('href')
+    vidHash = vidHash.split('v=')[1]
+    
+    return vidTitle.text, vidHash
 
 with open('channels.csv') as f:
     channels = f.readlines()
@@ -43,27 +48,27 @@ for i, channel in enumerate(channels):
         channel[2] += ',' + channel.pop(3)
     channelURL = 'https://www.youtube.com/' + channel[1] + '/videos'
     
-    #compare this old title with the newest video
-    newestVid = getVidFromChannel(channelURL)
-    #replace utf characters
-    newestVid = newestVid.encode('ascii', errors='ignore') #'replace' would make unicode
-    newestVid = newestVid.decode('ascii', errors='ignore').rstrip()      #chars into '?'
+    #compare the last saved video hash with the newest video's hash
+    newestTitle, newestHash = getVidFromChannel(channelURL)
+    #remove utf characters
+    newestTitle = newestTitle.encode('ascii', errors='ignore') #'replace' would make unicode
+    newestTitle = newestTitle.decode('ascii', errors='ignore').rstrip()      #chars into '?'
     if i % 10 == 0 and i != 0:
         print()    
-    print(f'{i:>2d}', end=' ')         #2 spaces for up to 2 digits, ">" = aligned right
-    if not channel[2] == newestVid:
-        print(f'\n{channel[0]} has a new video: {newestVid}')
+    print(f'{i:>2d}', end=' ')           #2 spaces for up to 2 digits, ">" = aligned right
+    if channel[2] != newestHash:
+        print(f'\n{channel[0]} has a new video: {newestTitle}')
         print(f'channel url: {channelURL}')
         if openURL:
             os.system(f'""{firefoxPath}" -new-tab "{channelURL}""')
-            #yes, os.system() needs those extra surrounding double quotes to correctly use 
+            #yes, os.system() needs those extra surrounding double quotes to correctly use
             #commands which contain a "
         
     #replace each last video with the most current last video  
-    channel[2] = newestVid
+    channel[2] = newestHash
     channel = ','.join(channel) + '\n'
     channels[i] = channel
-
+        
 driver.quit()
 #update the list
 with open('channels.csv', 'w') as f:
